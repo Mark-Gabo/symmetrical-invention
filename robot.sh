072dfb2f89fabca553333fdfa75c4df3975ed564
Mark Anthony Gabo <65881629+Mark-Gabo@users.noreply.github.com>
2020-08-16 06:07:53 +0000

    Add files via upload

---
+++ robot.sh
@@ -0,0 +1,13 @@
+echo '
+       __Greetings Peoples :)
+   _  |@@|
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "Script By:Mark Anthony Gabo"| lolcat
+echo "ft.Phillipine Termux Academy(PTA)"| lolcat
